0e190c4b895e6c4b052eb28dbb9f30ae54d87947
Andrew Andrew <fasgetz@hotmail.com>
2019-04-15 13:54:28 +0300

    па

---
+++ MyComputerStore/test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic kek = new System.Dynamic.ExpandoObject();
+
+            kek.Name = "Andrew";
+
+            kek.Age = 16;
+
+            List<dynamic> ts = new List<dynamic>();
+
+            ts.Add(kek);
+            ts.Add(kek);
+            ts.Add(kek);
+            ts.Add(kek);
+
+
+            Console.WriteLine(kek.Name);
+
+            string js = JsonConvert.SerializeObject(kek);
+
+            Console.WriteLine(js);
+
+
+        }
+    }
+}
